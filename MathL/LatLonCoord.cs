97f258ab4b6b86f4e9d77529b08492b4502f7c15
Dan Lodholm <dlodholm@icloud.com>
2022-11-02 17:08:06 -0600

    Added LatLonCoord class to better wrap Latitude, Longitude, Altitude coordinates

---
+++ MathL/LatLonCoord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace loki_bms_csharp.MathL
+{
+    public struct LatLonCoord
+    {
+        /// <summary>
+        /// Latitude, in Degrees Decimal
+        /// </summary>
+        public double Lat_Degrees
+        {
+            get { return Lat_Rad * MathL.ToDegrees; }
+            set { Lat_Rad = value * MathL.ToRadians; }
+        }
+        public double Lat_Rad;
+        /// <summary>
+        /// Longitude, in Degrees Decimal
+        /// </summary>
+        public double Lon_Degrees
+        {
+            get { return Lon_Rad * MathL.ToDegrees; }
+            set { Lon_Rad = value * MathL.ToRadians; }
+        }
+        public double Lon_Rad;
+        /// <summary>
+        /// Altitude relative to the surface of the reference sphere
+        /// </summary>
+        public double Alt;
+    }
+}
