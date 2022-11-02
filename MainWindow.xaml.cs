f985431ccf26f882432b2ae7ad431336cada4868
Dan Lodholm <dlodholm@icloud.com>
2022-11-01 23:09:38 -0600

    Rendering Pipeline
    Imported SkiaSharp libraries for rendering
    
    ScopeRenderer can display lines and orient itself based on camera view angle and zoom using Vector64 Coordinates

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using loki_bms_csharp.UserInterface;
+using SkiaSharp;
+using SkiaSharp.Views.WPF;
 
 namespace loki_bms_csharp
 {
@@ -23,11 +25,73 @@ namespace loki_bms_csharp
     public partial class MainWindow : Window
     {
         private Brush StrokeBrush = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-        private Brush FillBrush = new SolidColorBrush(Color.FromArgb(128,255, 0, 0));
+        private Brush FillBrush = new SolidColorBrush(Color.FromArgb(128, 255, 0, 0));
+
+        public int FPS { get; private set; } = 30;
+        public double MSPerFrame
+        {
+            get { return 1000 / FPS; }
+        }
+        private System.Timers.Timer RenderClock;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            BeginInit();
+
+            ConfigureClock();
+            ScopeCanvas.PaintSurface += OnPaintSurface;
+
+            EndInit();
+        }
+
+        private void ConfigureClock ()
+        {
+            RenderClock = new System.Timers.Timer();
+            RenderClock.Interval = MSPerFrame;
+            RenderClock.Elapsed += delegate (Object s, System.Timers.ElapsedEventArgs args)
+            {
+                try
+                {
+                    Dispatcher.Invoke(delegate ()
+                    {
+                        ScopeCanvas.InvalidateVisual();
+                    }, System.Windows.Threading.DispatcherPriority.Render);
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("Missed a frame due to an exception!\n" + e.Message);
+                }
+            };
+            RenderClock.Start();
+        }
+
+        private void OnPaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs args)
+        {
+            (double lat, double lon, double alt) = GetLatLonAltSliders();
+            MathL.TangentMatrix camMatrix = MathL.TangentMatrix.FromLatLon(lat, lon, false);
+
+            var renderer = new ScopeRenderer(args, camMatrix);
+            renderer.VerticalSize = alt;
+
+            renderer.DrawLine((0, 0, 0), (1, 0, 0), SKColors.Red, 3);
+            renderer.DrawLine((0, 0, 0), (0, 1, 0), SKColors.Green, 3);
+            renderer.DrawLine((0, 0, 0), (0, 0, 1), SKColors.Blue, 3);
+        }
+
+        public (double LA, double LO, double AL) GetLatLonAltSliders()
+        {
+            double lat = Lat_Slider.Value;
+            double lon = Lon_Slider.Value;
+            double alt = Zoom_Slider.Value;
+
+            return (lat, lon, alt);
+        }
+
+        private void PrimaryDisplay_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            RenderClock.Stop();
         }
     }
 }
---
+++ UserInterface/ScopeRenderer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SkiaSharp;
+using SkiaSharp.Views.WPF;
+
+namespace loki_bms_csharp.UserInterface
+{
+    class ScopeRenderer
+    {
+        public SKImageInfo Info { get; private set; }
+        public int Width
+        {
+            get
+            { return Info.Width; }
+        }
+        public int Height
+        {
+            get { return Info.Height; }
+        }
+
+        public SKSurface Surface { get; private set; }
+        public SKCanvas Canvas { get; private set; }
+        public MathL.TangentMatrix CameraMatrix;
+        public double VerticalSize = 2;
+        public double PixelsPerUnit
+        {
+            get { return Height / VerticalSize; }
+        }
+
+        public ScopeRenderer (SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs args, MathL.TangentMatrix cameraMatrix)
+        {
+            Info = args.Info;
+            Surface = args.Surface;
+            Canvas = Surface.Canvas;
+            CameraMatrix = cameraMatrix;
+
+            Canvas.Clear(SKColors.Black);
+        }
+
+        public void DrawLine (Vector64 from, Vector64 to, SKColor color, float width)
+        {
+            Vector64 localFrom = CameraMatrix.ToLocal(from) * PixelsPerUnit;
+            Vector64 localTo = CameraMatrix.ToLocal(to) * PixelsPerUnit;
+
+            SKPoint ptFrom = new SKPoint((float)localFrom.y + Width / 2, (float)localFrom.z + Height/2);
+            SKPoint ptTo = new SKPoint((float)localTo.y + Width / 2, (float)localTo.z + Height / 2);
+
+            SKPaint stroke = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = color,
+                StrokeWidth = width,
+            };
+
+            Canvas.DrawLine(ptFrom, ptTo, stroke);
+        }
+
+    }
+}
