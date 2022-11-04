1a519a93c176d6f87a1fbfd376d04f7f0030d812
Dan Lodholm <dlodholm@icloud.com>
2022-11-04 13:06:18 -0600

    Add Path3D and PathSVG classes to store and handle world landmass geometry and other paths

---
+++ UserInterface/Maps/Path3D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SkiaSharp;
+
+namespace loki_bms_csharp.UserInterface.Maps
+{
+    public class Path3D
+    {
+        public string Name = "New Path";
+        public Vector64[] Points;
+    }
+}
---
+++ UserInterface/Maps/PathSVG.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace loki_bms_csharp.UserInterface.Maps
+{
+    public class PathSVG
+    {
+        public string name;
+        public string data;
+
+        public char delimiter = 'Z';
+        public string[] Subpaths
+        {
+            get
+            {
+                if(data.Length > 0)
+                {
+                    string[] splits = data.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+                    return splits;
+                }
+                return new string[0];
+            }
+        }
+
+        public PathSVG[] Subdivide (char nextDelim = 'L')
+        {
+            var _subpaths = Subpaths;
+            PathSVG[] paths = new PathSVG[_subpaths.Length];
+
+            for (int i = 0; i < _subpaths.Length; i++)
+            {
+                paths[i] = new PathSVG { name = $"{this.name}_{i}", data = _subpaths[i], delimiter = nextDelim };
+            }
+
+            return paths;
+        }
+
+        public System.Windows.Point[] GetPoints ()
+        {
+            string[] possiblePoints = Subpaths;
+
+            List<System.Windows.Point> pointsList = new List<System.Windows.Point>(0);
+
+            foreach (string point in possiblePoints)
+            {
+                string cleaned = point.Replace("M", "").Replace("Z", "");
+                string[] split = cleaned.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                if (split.Length == 2)
+                {
+                    double x, y;
+                    bool x_success = double.TryParse(split[0], out x);
+                    bool y_success = double.TryParse(split[1], out y);
+
+                    if (x_success && y_success) pointsList.Add(new System.Windows.Point(x, y));
+                }
+            }
+
+            return pointsList.ToArray();
+        }
+    }
+}
