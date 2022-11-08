082532976e3a3359c87649c6b1072e158d59c96a
Dan Lodholm <dlodholm@icloud.com>
2022-11-08 16:23:02 -0700

    TrackDatum now has reference to Originating DataSource

--- Database/TrackDatum.cs
+++ Database/TrackDatum.cs
@@ -7,6 +7,7 @@ namespace loki_bms_csharp.Database
     public class TrackDatum : IKinematicData
     {
         public TrackNumber ID;
+        public DataSource Origin;
 
         public Vector64 Position { get; set; }
         public Vector64 Velocity { get; set; }
