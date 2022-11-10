be0f1e7db0b895f866bab504cdc38b7aa9c1f78f
Dan Lodholm <dlodholm@icloud.com>
2022-11-09 22:29:38 -0700

    Implemented Track Details Window

---
+++ Windows/TrackDetails.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace loki_bms_csharp.Windows
+{
+    /// <summary>
+    /// Interaction logic for TrackDetails.xaml
+    /// </summary>
+    public partial class TrackDetails : UserControl
+    {
+        public TrackDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void IDSelection_Loaded(object sender, RoutedEventArgs e)
+        {
+            IDSelection.ItemsSource = Enum.GetValues(typeof(Database.FriendFoeStatus)).Cast<Database.FriendFoeStatus>();
+        }
+    }
+}
