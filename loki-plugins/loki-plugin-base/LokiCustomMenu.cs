db711769582a84f5cc7b856df6bb3fb28e0738cb
orbitusii <dlodholm@icloud.com>
2023-06-13 17:05:34 -0600

    Add LokiPlugin base classes All classes are abstract, developers must implement behavior on a per-plugin basis

---
+++ loki-plugins/loki-plugin-base/LokiCustomMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loki_plugin_base
+{
+    public abstract class LokiCustomMenu
+    {
+        public string XamlSourcePath = string.Empty;
+        public object? GeneratedElements;
+        public string ButtonText = "???";
+    }
+}
