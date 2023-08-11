628ba70ad02877d4802d3415b563d49882f1ada4
Dani Lodholm <dlodholm@icloud.com>
2023-08-11 11:26:11 -0600

    Refactor ColorSettings for serializing stroke and fill colors Add GetPaint method to quickly and sanely retrieve the colors needed based on an ISelectableObject's properties - should be more robust and extensible to add features like Paint-by-IFF

--- Settings/ColorSettings.cs
+++ Settings/ColorSettings.cs
@@ -1,4 +1,5 @@
 ﻿using loki_bms_common.Database;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -41,63 +42,132 @@ namespace loki_bms_csharp.Settings
         public event PropertyChangedEventHandler? PropertyChanged;
 
         [XmlElement]
-        public string Stroke_Friend = "#FF0000FF";
+        public string? Stroke_Friend = "#FF0000FF";
         [XmlElement]
-        public string Stroke_AsFnd = "#FF008000";
+        public string? Stroke_AsFnd = "#FF008000";
         [XmlElement]
-        public string Stroke_Neutral = "#FF800080";
+        public string? Stroke_Neutral = "#FF800080";
         [XmlElement]
-        public string Stroke_Suspect = "#FFFFA500";
+        public string? Stroke_Suspect = "#FFFFA500";
         [XmlElement]
-        public string Stroke_Hostile = "#FFFF0000";
+        public string? Stroke_Hostile = "#FFFF0000";
         [XmlElement]
-        public string Stroke_Unknown = "#FFFFFF00";
+        public string? Stroke_Unknown = "#FFFFFF00";
         [XmlElement]
-        public string Stroke_Pending = "#FF808080";
+        public string? Stroke_Pending = "#FF808080";
 
         [XmlElement]
-        public string Fill_Friend = "#800000FF";
+        public string? Fill_Friend = "#800000FF";
         [XmlElement]
-        public string Fill_AsFnd = "#80008000";
+        public string? Fill_AsFnd = "#80008000";
         [XmlElement]
-        public string Fill_Neutral = "#80800080";
+        public string? Fill_Neutral = "#80800080";
         [XmlElement]
-        public string Fill_Suspect = "#80FFA500";
+        public string? Fill_Suspect = "#80FFA500";
         [XmlElement]
-        public string Fill_Hostile = "#80FF0000";
+        public string? Fill_Hostile = "#80FF0000";
         [XmlElement]
-        public string Fill_Unknown = "#80FFFF00";
+        public string? Fill_Unknown = "#80FFFF00";
         [XmlElement]
-        public string Fill_Pending = "#80808080";
+        public string? Fill_Pending = "#80808080";
 
-        [XmlIgnore]
-        public Dictionary<FriendFoeStatus, SkiaSharp.SKPaint> StrokeByFFS =
-            new Dictionary<FriendFoeStatus, SkiaSharp.SKPaint>()
-            {
-                {FriendFoeStatus.KnownFriend, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Stroke, Color = SkiaSharp.SKColors.Blue, StrokeWidth = 2 } },
-                {FriendFoeStatus.AssumedFriend, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Stroke, Color = SkiaSharp.SKColors.Green, StrokeWidth = 2 } },
-                {FriendFoeStatus.Neutral, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Stroke, Color = SkiaSharp.SKColors.Purple, StrokeWidth = 2 }  },
-                {FriendFoeStatus.Suspect, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Stroke, Color = SkiaSharp.SKColors.Orange, StrokeWidth = 2 }  },
-                {FriendFoeStatus.Hostile, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Stroke, Color = SkiaSharp.SKColors.Red, StrokeWidth = 2 }  },
-                {FriendFoeStatus.Unknown, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Stroke, Color = SkiaSharp.SKColors.Yellow, StrokeWidth = 2 }  },
-                {FriendFoeStatus.Pending, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Stroke, Color = SkiaSharp.SKColors.Gray, StrokeWidth = 2 }  },
-            };
-        [XmlIgnore]
-        public Dictionary<FriendFoeStatus, SkiaSharp.SKPaint> FillByFFS =
-            new Dictionary<FriendFoeStatus, SkiaSharp.SKPaint>()
-            {
-                {FriendFoeStatus.KnownFriend, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Fill, Color = SkiaSharp.SKColors.Blue.WithAlpha(128) } },
-                {FriendFoeStatus.AssumedFriend, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Fill, Color = SkiaSharp.SKColors.Green.WithAlpha(128) } },
-                {FriendFoeStatus.Neutral, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Fill, Color = SkiaSharp.SKColors.Purple.WithAlpha(128) }  },
-                {FriendFoeStatus.Suspect, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Fill, Color = SkiaSharp.SKColors.Orange.WithAlpha(128) }  },
-                {FriendFoeStatus.Hostile, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Fill, Color = SkiaSharp.SKColors.Red.WithAlpha(128) }  },
-                {FriendFoeStatus.Unknown, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Fill, Color = SkiaSharp.SKColors.Yellow.WithAlpha(128) }  },
-                {FriendFoeStatus.Pending, new SkiaSharp.SKPaint{Style = SkiaSharp.SKPaintStyle.Fill, Color = SkiaSharp.SKColors.Gray.WithAlpha(128) }  },
-            };
-
-        public override void OnLoad()
+        public enum PaintType
         {
-
+            Stroke,
+            Fill
         }
+
+        public (SKPaint stroke, SKPaint fill) GetPaint (ISelectableObject target)
+        {
+            var strokecolor = target.FFS switch
+            {
+                FriendFoeStatus.KnownFriend => SKColor.TryParse(Stroke_Friend, out var _out) ? _out : SKColors.Blue,
+                FriendFoeStatus.AssumedFriend => SKColor.TryParse(Stroke_AsFnd, out var _out) ? _out : SKColors.Green,
+                FriendFoeStatus.Neutral => SKColor.TryParse(Stroke_Neutral, out var _out) ? _out : SKColors.Purple,
+                FriendFoeStatus.Suspect => SKColor.TryParse(Stroke_Suspect, out var _out) ? _out : SKColors.Orange,
+                FriendFoeStatus.Hostile => SKColor.TryParse(Stroke_Hostile, out var _out) ? _out : SKColors.Red,
+                FriendFoeStatus.Unknown => SKColor.TryParse(Stroke_Unknown, out var _out) ? _out : SKColors.Yellow,
+                FriendFoeStatus.Pending => SKColor.TryParse(Stroke_Pending, out var _out) ? _out : SKColors.Yellow,
+                _ => SKColors.Gray
+            };
+
+            var fillcolor = (target.FFS switch
+            {
+                FriendFoeStatus.KnownFriend => SKColor.TryParse(Fill_Friend, out var _out) ? _out : SKColors.Blue,
+                FriendFoeStatus.AssumedFriend => SKColor.TryParse(Fill_AsFnd, out var _out) ? _out : SKColors.Green,
+                FriendFoeStatus.Neutral => SKColor.TryParse(Fill_Neutral, out var _out) ? _out : SKColors.Purple,
+                FriendFoeStatus.Suspect => SKColor.TryParse(Fill_Suspect, out var _out) ? _out : SKColors.Orange,
+                FriendFoeStatus.Hostile => SKColor.TryParse(Fill_Hostile, out var _out) ? _out : SKColors.Red,
+                FriendFoeStatus.Unknown => SKColor.TryParse(Fill_Unknown, out var _out) ? _out : SKColors.Yellow,
+                FriendFoeStatus.Pending => SKColor.TryParse(Fill_Pending, out var _out) ? _out : SKColors.Yellow,
+                _ => SKColors.Gray
+            }).WithAlpha(128);
+
+            SKPaint stroke;
+            SKPaint fill;
+
+            // Same color present in the cached stroke paint, reuse the cached one
+            if (cachedStroke.TryGetValue(target.FFS, out var _stroke) && _stroke.Color.Equals(strokecolor))
+            {
+                stroke = _stroke;
+            }
+            else
+            {
+                stroke = new SKPaint
+                {
+                    Style = SKPaintStyle.Stroke,
+                    Color = strokecolor,
+                    StrokeWidth = 2,
+                };
+                cachedStroke[target.FFS] = stroke;
+            }
+
+            // Same color present in the cached fill paint, reuse the cached one
+            if (cachedFill.TryGetValue(target.FFS, out var _fill) && _fill.Color.Equals(fillcolor))
+            {
+                fill = _fill;
+            }
+            else
+            {
+                fill = new SKPaint
+                {
+                    Style = SKPaintStyle.Fill,
+                    Color = fillcolor
+                };
+                cachedFill[target.FFS] = fill;
+            }
+
+            return (stroke, fill);
+        }
+
+        [XmlIgnore]
+        public Dictionary<FriendFoeStatus, SKPaint> cachedStroke = new Dictionary<FriendFoeStatus, SKPaint>();
+        [XmlIgnore]
+        public Dictionary<FriendFoeStatus, SKPaint> cachedFill = new Dictionary<FriendFoeStatus, SKPaint>();
+
+        [XmlIgnore]
+        public Dictionary<FriendFoeStatus, SKPaint> StrokeByFFS =
+            new Dictionary<FriendFoeStatus, SKPaint>()
+            {
+                {FriendFoeStatus.KnownFriend, new SKPaint{Style = SKPaintStyle.Stroke, Color = SKColors.Blue, StrokeWidth = 2 } },
+                {FriendFoeStatus.AssumedFriend, new SKPaint{Style = SKPaintStyle.Stroke, Color = SKColors.Green, StrokeWidth = 2 } },
+                {FriendFoeStatus.Neutral, new SKPaint{Style = SKPaintStyle.Stroke, Color = SKColors.Purple, StrokeWidth = 2 }  },
+                {FriendFoeStatus.Suspect, new SKPaint{Style = SKPaintStyle.Stroke, Color = SKColors.Orange, StrokeWidth = 2 }  },
+                {FriendFoeStatus.Hostile, new SKPaint{Style = SKPaintStyle.Stroke, Color = SKColors.Red, StrokeWidth = 2 }  },
+                {FriendFoeStatus.Unknown, new SKPaint{Style = SKPaintStyle.Stroke, Color = SKColors.Yellow, StrokeWidth = 2 }  },
+                {FriendFoeStatus.Pending, new SKPaint{Style = SKPaintStyle.Stroke, Color = SKColors.Gray, StrokeWidth = 2 }  },
+            };
+        [XmlIgnore]
+        public Dictionary<FriendFoeStatus, SKPaint> FillByFFS =
+            new Dictionary<FriendFoeStatus, SKPaint>()
+            {
+                {FriendFoeStatus.KnownFriend, new SKPaint{Style = SKPaintStyle.Fill, Color = SKColors.Blue.WithAlpha(128) } },
+                {FriendFoeStatus.AssumedFriend, new SKPaint{Style = SKPaintStyle.Fill, Color = SKColors.Green.WithAlpha(128) } },
+                {FriendFoeStatus.Neutral, new SKPaint{Style = SKPaintStyle.Fill, Color = SKColors.Purple.WithAlpha(128) }  },
+                {FriendFoeStatus.Suspect, new SKPaint{Style = SKPaintStyle.Fill, Color = SKColors.Orange.WithAlpha(128) }  },
+                {FriendFoeStatus.Hostile, new SKPaint{Style = SKPaintStyle.Fill, Color = SKColors.Red.WithAlpha(128) }  },
+                {FriendFoeStatus.Unknown, new SKPaint{Style = SKPaintStyle.Fill, Color = SKColors.Yellow.WithAlpha(128) }  },
+                {FriendFoeStatus.Pending, new SKPaint{Style = SKPaintStyle.Fill, Color = SKColors.Gray.WithAlpha(128) }  },
+            };
     }
 }
