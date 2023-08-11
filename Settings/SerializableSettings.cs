d7358c7573ebafb070c117095b5bb6847884a7c7
Dani Lodholm <dlodholm@icloud.com>
2023-08-11 10:42:31 -0600

    Refactor ColorSettings to eliminate need to reimplement SaveToFile Instead, derived classes have to override "Original" to give the generic a reference to the original object

--- Settings/SerializableSettings.cs
+++ Settings/SerializableSettings.cs
@@ -11,6 +11,8 @@ namespace loki_bms_csharp.Settings
 {
     public abstract class SerializableSettings<T>
     {
+        public abstract T Original { get; }
+
         public static T? LoadFromFile(string filename)
         {
             if(File.Exists(filename))
@@ -20,10 +22,20 @@ namespace loki_bms_csharp.Settings
                     XmlSerializer ser = new XmlSerializer(typeof(T));
                     var result = (T)ser.Deserialize(stream);
 
-                    if (result is SerializableSettings<T>) return result;
+                    if (result is SerializableSettings<T> loaded)
+                    {
+                        loaded.OnLoad();
+                        return result;
+                    }
                 }
             }
             return default;
+        }
+
+        public virtual void OnLoad() { }
+        public virtual void SaveToFile (string filename)
+        {
+            SaveToFile(filename, Original);
         }
 
         public static void SaveToFile (string filename, T settings)
