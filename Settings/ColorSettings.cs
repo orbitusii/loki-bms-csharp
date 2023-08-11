1fd238c4e0d06bfec19a75ef75567c6c6a5a5ec7
Dani Lodholm <dlodholm@icloud.com>
2023-08-11 16:09:28 -0600

    Refactor ColorSettings to use Properties for use by UI

--- Settings/ColorSettings.cs
+++ Settings/ColorSettings.cs
@@ -39,37 +39,165 @@ namespace loki_bms_csharp.Settings
             }
         }
 
+        [XmlElement]
+        public string? Stroke_Friend
+        {
+            get => stroke_Friend;
+            set
+            {
+                stroke_Friend = value;
+                PropertyChanged?.Invoke(this,new(nameof(Stroke_Friend)));
+            }
+        }
+        [XmlElement]
+        public string? Stroke_AsFnd
+        {
+            get => stroke_AsFnd;
+            set
+            {
+                stroke_AsFnd = value; 
+                PropertyChanged?.Invoke(this, new(nameof(Stroke_AsFnd)));
+            }
+
+        }
+        [XmlElement]
+        public string? Stroke_Neutral
+        {
+            get => stroke_Neutral;
+            set
+            {
+                stroke_Neutral = value;
+                PropertyChanged?.Invoke(this, new(nameof(Stroke_Neutral)));
+            }
+        }
+        [XmlElement]
+        public string? Stroke_Suspect
+        {
+            get => stroke_Suspect;
+            set
+            {
+                stroke_Suspect = value;
+                PropertyChanged?.Invoke(this, new(nameof(Stroke_Suspect)));
+            }
+        }
+        [XmlElement]
+        public string? Stroke_Hostile
+        {
+            get => stroke_Hostile;
+            set
+            {
+                stroke_Hostile = value;
+                PropertyChanged?.Invoke(this, new(nameof(Stroke_Hostile)));
+            }
+        }
+        [XmlElement]
+        public string? Stroke_Unknown
+        {
+            get => stroke_Unknown;
+            set
+            {
+                stroke_Unknown = value;
+                PropertyChanged?.Invoke(this, new(nameof(Stroke_Unknown)));
+            }
+        }
+        [XmlElement]
+        public string? Stroke_Pending
+        {
+            get => stroke_Pending;
+            set
+            {
+                stroke_Pending = value;
+                PropertyChanged?.Invoke(this, new(nameof(Stroke_Pending)));
+            }
+        }
+        [XmlElement]
+        public string? Fill_Friend
+        {
+            get => fill_Friend;
+            set
+            {
+                fill_Friend = value;
+                PropertyChanged?.Invoke(this, new(nameof(Fill_Friend)));
+            }
+        }
+        [XmlElement]
+        public string? Fill_AsFnd
+        {
+            get => fill_AsFnd;
+            set
+            {
+                fill_AsFnd = value;
+                PropertyChanged?.Invoke(this, new(nameof(Fill_AsFnd)));
+            }
+        }
+        [XmlElement]
+        public string? Fill_Neutral
+        {
+            get => fill_Neutral;
+            set
+            {
+                fill_Neutral = value;
+                PropertyChanged?.Invoke(this, new(nameof(Fill_Neutral)));
+            }
+        }
+        [XmlElement]
+        public string? Fill_Suspect
+        {
+            get => fill_Suspect;
+            set
+            {
+                fill_Suspect = value;
+                PropertyChanged?.Invoke(this, new(nameof(Fill_Suspect)));
+            }
+        }
+        [XmlElement]
+        public string? Fill_Hostile
+        {
+            get => fill_Hostile;
+            set
+            {
+                fill_Hostile = value;
+                PropertyChanged?.Invoke(this, new(nameof(Fill_Hostile)));
+            }
+        }
+        [XmlElement]
+        public string? Fill_Unknown
+        {
+            get => fill_Unknown;
+            set
+            {
+                fill_Unknown = value;
+                PropertyChanged?.Invoke(this, new(nameof(Fill_Unknown)));
+            }
+        }
+        [XmlElement]
+        public string? Fill_Pending
+        {
+            get => fill_Pending;
+            set
+            {
+                fill_Pending = value;
+                PropertyChanged?.Invoke(this, new(nameof(Fill_Pending)));
+            }
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        [XmlElement]
-        public string? Stroke_Friend = "#FF0000FF";
-        [XmlElement]
-        public string? Stroke_AsFnd = "#FF008000";
-        [XmlElement]
-        public string? Stroke_Neutral = "#FF800080";
-        [XmlElement]
-        public string? Stroke_Suspect = "#FFFFA500";
-        [XmlElement]
-        public string? Stroke_Hostile = "#FFFF0000";
-        [XmlElement]
-        public string? Stroke_Unknown = "#FFFFFF00";
-        [XmlElement]
-        public string? Stroke_Pending = "#FF808080";
+        private string? stroke_Friend = "#FF0000FF";
+        private string? stroke_AsFnd = "#FF008000";
+        private string? stroke_Neutral = "#FF800080";
+        private string? stroke_Suspect = "#FFFFA500";
+        private string? stroke_Hostile = "#FFFF0000";
+        private string? stroke_Unknown = "#FFFFFF00";
+        private string? stroke_Pending = "#FF808080";
 
-        [XmlElement]
-        public string? Fill_Friend = "#800000FF";
-        [XmlElement]
-        public string? Fill_AsFnd = "#80008000";
-        [XmlElement]
-        public string? Fill_Neutral = "#80800080";
-        [XmlElement]
-        public string? Fill_Suspect = "#80FFA500";
-        [XmlElement]
-        public string? Fill_Hostile = "#80FF0000";
-        [XmlElement]
-        public string? Fill_Unknown = "#80FFFF00";
-        [XmlElement]
-        public string? Fill_Pending = "#80808080";
+        private string? fill_Friend = "#800000FF";
+        private string? fill_AsFnd = "#80008000";
+        private string? fill_Neutral = "#80800080";
+        private string? fill_Suspect = "#80FFA500";
+        private string? fill_Hostile = "#80FF0000";
+        private string? fill_Unknown = "#80FFFF00";
+        private string? fill_Pending = "#80808080";
 
         public enum PaintType
         {
