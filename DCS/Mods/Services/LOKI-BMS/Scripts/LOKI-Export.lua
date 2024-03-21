87f5c43da3ed7b0e93710c63cb033b3dc8f62b97
Robin Lodholm <dlodholm@icloud.com>
2024-03-21 11:34:50 -0600

    Make LOKI-Export.lua functional and export data - Had to fix a bunch of stuff and add some helper methods, but it works!

--- DCS/Mods/Services/LOKI-BMS/Scripts/LOKI-Export.lua
+++ DCS/Mods/Services/LOKI-BMS/Scripts/LOKI-Export.lua
@@ -1,4 +1,4 @@
-local JSON = loadfile("Scripts\\JSON.lua")
+local JSON = loadfile("Scripts\\JSON.lua")()
 local socket = require("socket")
 
 local LOKI = {}
@@ -23,6 +23,44 @@ LOKI.UdpCommand:settimeout(0)
 LOKI.UdpUtility:setsockname("*", LOKI.UtilityPort)
 LOKI.UdpUtility:settimeout(0)
 
+function LOKI.GetAllUnits ()
+    --local bluefor = coalition.GetGroups(2, 0)
+    --local redfor = coalition.GetGroups(1,0)
+    --local neufor = coalition.GetGroups(0,0)
+
+    --local bluredmerge = LOKI.MergeTables(bluefor, redfor)
+    local rawUnits = LoGetWorldObjects()
+
+    local units = {}
+    for k,v in pairs(rawUnits) do
+        local temp = {
+            name = v.UnitName,
+            group = v.GroupName,
+            position = v.LatLongAlt,
+        }
+        table.insert(units, temp)
+    end
+
+    return units
+end
+
+function LOKI.GetUnitSet (_number, _offset)
+
+end
+
+function LOKI.MergeTables (_t1, _t2)
+    local merged = {}
+
+    for _, item in pairs(_t1) do
+        table.insert(merged, item)
+    end
+    for _, item in pairs(_t2) do
+        table.insert(merged, item)
+    end
+
+    return merged
+end
+
 LuaExportActivityNextEvent = function(tCurrent)
     local commands = LOKI.UdpCommand:receive()
     local utils = LOKI.UdpUtility:receive()
@@ -34,9 +72,20 @@ LuaExportActivityNextEvent = function(tCurrent)
         utils = "none"
     end
 
-    local message = "Test: \n"
+    local message = {
+        Timestamp = tCurrent,
+        isExportAllowed = LoIsObjectExportAllowed(),
+        Units = {},
+        OtherText = "some other text",
+    }
 
-    socket.try(LOKI.UdpExport:sendto(message, "127.0.0.1", LOKI.ExportPort))
+    if (LoIsObjectExportAllowed()) then
+        message.Units = LOKI.GetAllUnits()
+    end
+
+    local encoded = LOKI.JSON:encode(message)
+
+    socket.try(LOKI.UdpExport:sendto(encoded .. " \n", "127.0.0.1", LOKI.ExportPort))
 
     return tCurrent + 1.0
 end
